1258abfc2a7fa863a61669555020474e9e76e306
Magic_RB <magic_rb@redalder.org>
2021-08-30 13:39:13 +0200

    Fix app definition
    Signed-off-by: Magic_RB <magic_rb@redalder.org>

--- flake.nix
+++ flake.nix
@@ -10,50 +10,56 @@
       forAllSystems' = systems: fun: nixpkgs.lib.genAttrs systems fun;
       forAllSystems = forAllSystems' supportedSystems;
     in
-    {
-      overlays.pmbootstrap =
-        final: prev:
-        {
-          pmbootstrap = final.python3Packages.callPackage ./pmbootstrap.nix {};
-        };
-      
-      overlay = self.overlays.pmbootstrap;
-
-      packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs
-            { inherit system;
-              overlays = mapAttrsToList (_: id) self.overlays;
-            };
-        in
+      {
+        overlays.pmbootstrap =
+          final: prev:
           {
-            inherit (pkgs) pmbootstrap;
-          }
-      );
+            pmbootstrap = final.python3Packages.callPackage ./pmbootstrap.nix {};
+          };
+        
+        overlay = self.overlays.pmbootstrap;
 
-      defaultPackage = forAllSystems (system:
-        self.packages.${system}.pmbootstrap
-      );
+        packages = forAllSystems (system:
+          let
+            pkgs = import nixpkgs
+              { inherit system;
+                overlays = mapAttrsToList (_: id) self.overlays;
+              };
+          in
+            {
+              inherit (pkgs) pmbootstrap;
+            }
+        );
 
-      apps = self.packages;
+        apps = mapAttrs (_: v:
+          mapAttrs (_: a:
+            {
+              type = "app";
+              program = a;
+            }
+          ) v
+        ) self.packages;
 
-      defaultApp = self.defaultPackage;
+        defaultApp = mapAttrs (_: v:
+          v.pmbootstrap
+        ) self.apps;
 
-      devShell = forAllSystems (system:
-        let
-          pkgs = import nixpkgs
-            { inherit system;
-              overlays = mapAttrsToList (_: id) self.overlays;
-            };
-        in
-          pkgs.mkShell {
-            nativeBuildInputs = with pkgs;
-              [ (python3.withPackages (pkgs: with pkgs;
-                [ flake8 ]))
 
-                openssl git sudo
-              ];
-          }
-      );
-    };
+        devShell = forAllSystems (system:
+          let
+            pkgs = import nixpkgs
+              { inherit system;
+                overlays = mapAttrsToList (_: id) self.overlays;
+              };
+          in
+            pkgs.mkShell {
+              nativeBuildInputs = with pkgs;
+                [ (python3.withPackages (pkgs: with pkgs;
+                  [ flake8 ]))
+
+                  openssl git sudo
+                ];
+            }
+        );
+      };
 }
